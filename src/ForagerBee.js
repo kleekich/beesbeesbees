6320f74cabd452f23dc7e05cd412334877592330
Kangsik Lee <kangsik.kevin.lee@gmail.com>
2018-09-24 15:08:49 -0700

    Implements Bees

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,15 @@
 var ForagerBee = function() {
+	HoneyMakerBee.call(this);
+	this.age = 10;
+	this.job = 'find pollen';
 };
+
+
+ForagerBee.prototype = Object.create(HoneyMakerBee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
+ForagerBee.prototype.canFly = true;
+ForagerBee.prototype.treasureChest = [];
+ForagerBee.prototype.forage = function(item){
+	this.treasureChest.push(item);
+}
 
